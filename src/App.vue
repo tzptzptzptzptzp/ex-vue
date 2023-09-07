a40605ad8aebe4a4061d3273587f53d6ec515591
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-08 00:24:36 +0900

    ⚗️ 入力された値を表示する

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
-<script setup>
-import { computed } from 'vue'
-const answer = computed(() => {
-  return 1 + 1
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const message = computed(() => {
+  return 'ようこそ <strong>' + myName.value + '</strong>さん！'
 })
-const message = 'ようこそ <strong>〇〇</strong>さん！'
 const myColor = 'red'
+const myName = ref('')
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen">
-    <div>
+    <div class="flex flex-col items-center gap-4">
+      <div>
+        <p>お名前は？： <input v-model="myName" type="text" class="px-1 border" /></p>
+      </div>
       <p v-html="message" :style="{ color: myColor, fontSize: '2rem' }"></p>
     </div>
   </div>
