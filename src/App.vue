3c78015987501eb1327db9e7c4fe5da9029b4b72
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-08 00:05:35 +0900

    ⚗️ v-htmlで変数を表示する

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import { computed } from 'vue'
 const answer = computed(() => {
   return 1 + 1
 })
+const message = 'ようこそ <strong>〇〇</strong>さん！'
 </script>
 
 <template>
-  <div class="flex">
-    <p>1 + 1 = {{ answer }}</p>
+  <div class="flex items-center justify-center min-h-screen">
+    <div>
+      <p v-html="message"></p>
+    </div>
   </div>
 </template>
