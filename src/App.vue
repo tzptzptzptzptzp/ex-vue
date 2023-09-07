e5e2deaabeda7f0b300ca9f32e8678cc16a6d3ea
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-08 00:11:33 +0900

    ⚗️ v-bindでスタイルを調整

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@ const answer = computed(() => {
   return 1 + 1
 })
 const message = 'ようこそ <strong>〇〇</strong>さん！'
+const myColor = 'red'
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen">
     <div>
-      <p v-html="message"></p>
+      <p v-html="message" :style="{ color: myColor, fontSize: '2rem' }"></p>
     </div>
   </div>
 </template>
