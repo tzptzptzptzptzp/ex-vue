007c68cec5fefdb8ac4e4bf5f1eb24778fce8d4a
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-08 23:50:16 +0900

    ⚗️ TodoListを作成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const number = ref(0)
 
-const add = () => {
-  number.value++
+const todos = ref<string[]>([])
+const newTodo = ref<string>('')
+const addTodo = () => {
+  todos.value.push(newTodo.value)
+  newTodo.value = ''
 }
 </script>
 
 <template>
-  <div class="flex items-center justify-center gap-2 min-h-screen">
-    <input type="text" v-model="number" class="border" size="30" />
-    <button @click="add" class="px-2 bg-gray-300 text-white">+1</button>
+  <div class="flex flex-col items-center justify-center gap-2 min-h-screen">
+    <div class="flex gap-2">
+      <input type="text" v-model="newTodo" class="border" size="30" />
+      <button @click="addTodo()" class="px-2 bg-gray-300 text-white">追加</button>
+    </div>
+
+    <ul>
+      <li v-for="(todo, i) in todos" :key="i">{{ i }}. {{ todo }}</li>
+    </ul>
   </div>
 </template>
