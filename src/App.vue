b9a7254f6a8f6043447b99ecacf3c5dfcbb77938
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-08 23:30:16 +0900

    ⚗️ カウンターを実装

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,15 @@
-<template></template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const number = ref(0)
+
+const add = () => {
+  number.value++
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center gap-2 min-h-screen">
+    <input type="text" v-model="number" class="border" size="30" />
+    <button @click="add" class="px-2 bg-gray-300 text-white">+1</button>
+  </div>
+</template>
